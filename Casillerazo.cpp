a0ffe97123f8de274eaadd841e084372e564396e
Jesus1102 <jesus60302@gmail.com>
2024-02-06 12:42:17 -0500

    Agregue Casillerazo.cpp

---
+++ Casillerazo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<time.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+    srand(time(NULL));
+
+    int random, Eleccion;
+
+    random = rand()%2;
+    
+    cout << random << endl;
+
+    cout << "Eliga una opcion:" << endl;
+    cout << "0. para cara" << endl;
+    cout << "1. para sello" << endl;
+    cin >> Eleccion ;
+
+    
+
+    if (Eleccion == random){
+
+        cout << "Felicidades, Ganaste" << endl;
+
+    }
+    else{
+
+        cout << "Lo siento, Perdiste" << endl;
+    }
+    return 0;
+
+}
