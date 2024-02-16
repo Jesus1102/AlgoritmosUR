3e4eef78ea7a34ac1f511aee5b276c0741854d7c
Jesus1102 <159170739+Jesus1102@users.noreply.github.com>
2024-02-16 14:07:58 -0500

    Update Punto6.cpp

---
+++ TALLER 0/Punto6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i = 0, suma = 0;
+    
+    while (i < 100){
+        i++;
+        suma = suma + i;
+        
+    }
+    
+    cout << suma << endl;
+
+    return 0;
+
+}
